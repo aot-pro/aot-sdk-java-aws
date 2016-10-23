48d68fa4b1fd4bb774635a6cada01cbc9efdad47
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2016-10-23 19:13:23 +0300

    added new storage API stab

--- src/main/java/aot/storage/s3/CustomStorage.java
+++ src/main/java/aot/storage/s3/CustomStorage.java
@@ -18,9 +18,15 @@
 package aot.storage.s3;
 
 import aot.storage.Storage;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 
-import java.util.regex.Pattern;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Map;
 
 /**
  * @author Dmitry Kotlyarov
@@ -28,6 +34,10 @@ import java.util.regex.Pattern;
  */
 public class CustomStorage extends Storage {
     protected final AmazonS3 s3;
+
+    public CustomStorage(URL url, String[] ids) {
+        this(url.getHost(), url.getPath(), createS3(ids));
+    }
 
     public CustomStorage(String bucket, String prefix, AmazonS3 s3) {
         super(bucket, prefix);
@@ -40,12 +50,7 @@ public class CustomStorage extends Storage {
     }
 
     @Override
-    public Storage substorage(String prefix) {
-        return null;
-    }
-
-    @Override
-    public Iterable<String> find(String prefix) {
+    public Storage getSubstorage(String prefix) {
         return null;
     }
 
@@ -55,18 +60,28 @@ public class CustomStorage extends Storage {
     }
 
     @Override
-    public Iterable<String> find(String prefix, Pattern filter) {
+    public Iterable<String> list(String prefix, String filter) {
         return null;
     }
 
     @Override
-    public byte[] get(String key) {
+    public byte[] get(String key, Map<String, String> meta) {
         return new byte[0];
     }
 
     @Override
-    public void put(String key, byte[] data) {
+    public InputStream getStream(String key, Map<String, String> meta) {
+        return null;
+    }
 
+    @Override
+    public long put(String key, byte[] data, Map<String, String> meta) {
+        return 0;
+    }
+
+    @Override
+    public long upload(String key, InputStream input, long size, Map<String, String> meta) {
+        return 0;
     }
 
     @Override
@@ -75,17 +90,29 @@ public class CustomStorage extends Storage {
     }
 
     @Override
-    public String publish(String key) {
+    public String getUrl(String key) {
         return null;
     }
 
     @Override
-    public void hide(String key) {
-
+    public String getHttpsUrl(String key) {
+        return null;
     }
 
-    @Override
-    public String url(String key) {
-        return null;
+    protected static AmazonS3 createS3(String[] ids) {
+        AmazonS3 s3;
+        if ((ids.length >= 1) && !ids[1].trim().isEmpty()) {
+            String[] creds = ids[1].split(":");
+            s3 = new AmazonS3Client(new BasicAWSCredentials(creds[0], creds[1]));
+        } else {
+            s3 = new AmazonS3Client();
+        }
+        if ((ids.length >= 2) && !ids[2].trim().isEmpty()) {
+            s3.setRegion(Region.getRegion(Regions.fromName(ids[2])));
+        }
+        if ((ids.length >= 3) && !ids[3].trim().isEmpty()) {
+            s3.setEndpoint(ids[3]);
+        }
+        return s3;
     }
 }
